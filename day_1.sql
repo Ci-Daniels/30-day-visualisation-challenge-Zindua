839e9f9b9da9e12358b087bcaa0be59311c95298
Ci-Daniels <cikudaniels99@gmail.com>
2024-09-11 19:35:29 +0300

    Updated comenting on the script

--- day_1.sql
+++ day_1.sql
@@ -1,10 +1,10 @@
-# create database
+-- create database
 create database school;
 
-# use the created database
+-- use the created database
 Use school;
 
-# create table
+-- create table
 CREATE TABLE Students (
 StudentID INT PRIMARY KEY,
 FirstName VARCHAR(100) NOT NULL,
@@ -13,19 +13,19 @@ Age INT check (Age > 18),
 Email  VARCHAR(255) NOT NULL UNIQUE
 );
 
-# add values into table
+-- add values into table
 INSERT INTO Students (StudentID, FirstName, LastName, Age, Email)
 values (1 , 'John' , 'Doe','20' ,'john.dor@example.com'),
 (2, 'Jane', 'Smith' , '22', 'jane.smith@example.com'),
 (3, 'Michael' ,'Brown' ,'19',  'michael.brown@example.com');
 
-# display table details
+-- display table details
 SELECT * FROM  Students;
 
-# alter the table
-# 1. Add a new column named EnrollmentDate
-# 2. Add the max length to 300 characters
-# 3. Drop column LastName
+-- alter the table
+-- 1. Add a new column named EnrollmentDate
+-- 2. Add the max length to 300 characters
+-- 3. Drop column LastName
 ALTER TABLE Students
   ADD COLUMN EnrollmentDate DATE,
   MODIFY COLUMN Email  VARCHAR (300) NOT NULL UNIQUE,
